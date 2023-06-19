c84ddfdfe6507e0cc95d88cb780e28d3365e9ce7
muthugopaltcsens <muthu.gopal@tcsens.com>
2023-06-19 12:13:39 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
